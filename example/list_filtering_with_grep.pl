74b61782af975775491724b1ae463ddc6d5f5576
Saydul Bashar <tamimnoone@gmail.com>
2015-08-01 19:08:52 +0100

    Added Carp to remove Perlcritic warning by using croak.

--- example/list_filtering_with_grep.pl
+++ example/list_filtering_with_grep.pl
@@ -1,9 +1,13 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Data::Dumper;
+use Readonly;
+use Carp;
 
+Readonly my $SIZE => 10;
 my @input_numbers = qw(1 2 4 8 16 32 64);
-my @bigger_than_10 = grep {$_ > 10} @input_numbers;
+my @bigger_than_size = grep {$_ > $SIZE} @input_numbers;
 
-print Dumper(@bigger_than_10);
+foreach my $number (@bigger_than_size) {
+    print "$number\n" or croak 'print did not work';
+}
