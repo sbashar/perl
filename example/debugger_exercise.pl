3a1bbdac9b00046a7f5222604d27679f34443d16
Saydul Bashar <tamimnoone@gmail.com>
2015-08-01 21:20:57 +0100

    Reformatted the file and removed all the errors.

--- example/debugger_exercise.pl
+++ example/debugger_exercise.pl
@@ -1,7 +1,10 @@
-    #!/usr/bin/perl
-    use strict;
-    use warnings;
-    $var1 = 'Hello World';    # always wanted to do that :-)
-    $var2 = "$varl\n";
-    print $var2;
-    exit;
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Carp;
+
+my $var1 = 'Hello World';    # always wanted to do that :-)
+my $var2 = "$var1\n";
+
+print $var2 or croak 'print died';
+1;
