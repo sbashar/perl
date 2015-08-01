2726c033ec68c5ec73df6fefafebd5dc27ac79a4
Saydul Bashar <tamimnoone@gmail.com>
2015-08-01 23:12:33 +0100

    Fixed the issue with not quoting the hashes appropriately

--- example/debugger_exercise.pl
+++ example/debugger_exercise.pl
@@ -7,4 +7,15 @@ my $var1 = 'Hello World';    # always wanted to do that :-)
 my $var2 = "$var1\n";
 
 print $var2 or croak 'print died';
+
+my $key  = 'welcome';
+my %data = (
+    'this'    => qw(that),
+    'tom'     => q(and jerry),
+    'welcome' => q(Hello World),
+    'zip'     => q(welcome),
+);
+my @data = keys %data;
+print "$data{$key}\n" or croak 'print died';
+
 1;
